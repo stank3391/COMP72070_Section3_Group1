424e3c93c49bb49986b08f01b4aa5557546e6629
Sean <stank3391@conestogac.on.ca>
2024-01-15 08:39:15 -0500

    Add project files.

---
+++ COMP72070_Section3_Group1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP72070_Section3_Group1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
