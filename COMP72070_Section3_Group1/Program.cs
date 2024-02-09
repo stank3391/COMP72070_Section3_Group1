65bad15c17493dd6b8b61adbc96a3d7de7793a94
Bowen Sittler <bsittler9691@conestogac.on.ca>
2024-02-09 18:33:34 -0500

    Changed home page to loginwgoogle. User sees login screen first.

--- COMP72070_Section3_Group1/Program.cs
+++ COMP72070_Section3_Group1/Program.cs
@@ -29,6 +29,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=loginwgoogle}/{id?}");
 
 app.Run();
