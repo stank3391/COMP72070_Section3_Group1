196b61bcc49680f3b234d3dc9c58e317b9f7af36
ywang0789 <ywang0789@conestogac.on.ca>
2024-03-07 04:26:28 -0500

    moved tcpserver to its own class

---
+++ TcpServer/TcpServer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class TcpServer
+{
+    private TcpListener listener;
+    private int port;
+
+    private IPAddress localAddr = IPAddress.Any;
+    // private IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+    public TcpServer()
+    {
+        this.port = 27000;
+        this.listener = new TcpListener(this.localAddr, this.port);
+        this.listener.Start();
+    }
+
+    public void Start()
+    {
+        byte[] bytes = new byte[256];
+        String data;
+
+        while (true)
+        {
+            Console.WriteLine("waiting... ");
+            TcpClient client = this.listener.AcceptTcpClient();
+            Console.WriteLine("connected!");
+
+            data = "";
+
+            NetworkStream stream = client.GetStream();
+
+            int i;
+            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                data = Encoding.ASCII.GetString(bytes, 0, i);
+                Console.WriteLine("received: " + data);
+
+                data = "i have it";
+                byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                stream.Write(msg, 0, msg.Length);
+                Console.WriteLine("sent: " + data);
+            }
+
+            client.Close();
+        }
+    }
+
+}
+
