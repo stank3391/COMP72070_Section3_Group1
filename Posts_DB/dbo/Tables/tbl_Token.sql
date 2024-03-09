bd1c025384cf42440b32277f95b7b5a73f3f209e
NicolasMCompSci <115744859+NicolasMCompSci@users.noreply.github.com>
2024-03-09 11:37:49 -0500

    Finished database creation.

---
+++ Posts_DB/dbo/Tables/tbl_Token.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tbl_Token] (
+    [TokenId]        INT            NOT NULL,
+    [UserId]         INT            NOT NULL,
+    [Provider]       NVARCHAR (255) NULL,
+    [AccessToken]    CHAR (255)     NULL,
+    [RefreshToken]   CHAR (255)     NULL,
+    [CreationDate]   DATETIME       NULL,
+    [ExpirationDate] DATETIME       NULL,
+    PRIMARY KEY CLUSTERED ([TokenId] ASC),
+    CONSTRAINT [TokenUserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[tbl_Users] ([UserId])
+);
+
