d0d2c984a68ff9c7541023afd6f16bb9316a088e
Justin Schulz <jschulz8138@conestogac.on.ca>
2024-03-16 14:47:34 -0400

    Implemented Excel handling in tcp server

--- TcpServer/TcpServer.cs
+++ TcpServer/TcpServer.cs
@@ -8,6 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using COMP72070_Section3_Group1.Models;
 using COMP72070_Section3_Group1.Visitors;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Wordprocessing;
+using excel;
 
 namespace TcpServer
 {
@@ -41,6 +46,9 @@ namespace TcpServer
         /// </summary>
         public void Start()
         {
+            InsertText("test.xlsx", "Timestamp", "A", 1);
+            InsertText("test.xlsx", "User", "B", 1);
+            InsertText("test.xlsx", "Action", "C", 1);
             Console.WriteLine("Server started.");
             while (!this.isStop)
             {
@@ -92,24 +100,32 @@ namespace TcpServer
         {
             Console.WriteLine($"Packet received:\n{packet.ToString()}");
             Packet.Type type = packet.header.messageType;
-            
+
+            //InsertText("test.xlsx", "test", "E", 7);
+
+
             switch (type)
             {
                 case Packet.Type.Ack:
-                    Console.WriteLine("Ack received");
+                    Console.WriteLine("Ack received"); 
+                    CreateLog("test.xlsx", "yao", "Acknowledgement Signal Recieved");
                     break;
                 case Packet.Type.Error:
                     Console.WriteLine("Error received");
+                    CreateLog("test.xlsx", "yao", "Error Signal Recieved");
                     break;
                 case Packet.Type.Ready:
                     Console.WriteLine("Ready received");
+                    CreateLog("test.xlsx", "yao", "Ready Signal Recieved");
                     HandleReadyPacket();
                     break;
                 case Packet.Type.Auth:
                     Console.WriteLine("Auth received");
+                    CreateLog("test.xlsx", "yao", "Authorization Signal Recieved");
                     break;
                 default:
                     Console.WriteLine("Unknown packet type received");
+                    CreateLog("test.xlsx", "yao", "Unknown Signal Recieved");
                     break;
             }
         }
@@ -165,8 +181,229 @@ namespace TcpServer
             Console.WriteLine("Posts list updated from 'Database'.");
 
         }
+
+
+
+
+
+
+        static void CreateLog(string filename, string username, string action)
+        {
+            DateTime now = DateTime.Now;
+            //string name = now.ToString("dd/MM/yyyy");
+            string time = now.ToString("h:mm:ss tt");
+            InsertText(filename, time, "A", GetNextEmptyCell(filename, "Sheet1", "A"));
+            InsertText(filename, username, "B", GetNextEmptyCell(filename, "Sheet1", "B"));
+            InsertText(filename, action, "C", GetNextEmptyCell(filename, "Sheet1", "C"));
+        }
+
+
+
+        //static string CreateWorksheetForToday(string filename)
+        //{
+        //    using (SpreadsheetDocument document = SpreadsheetDocument.Open(filename, false))
+        //    {
+        //        WorkbookPart workbookPart = document.WorkbookPart;
+        //        DateTime now = DateTime.Now;
+        //        string name = now.ToString("dd/MM/yyyy");
+
+        //        //if ()
+        //        //{
+
+        //        //}
+
+
+        //        // Add a new worksheet part to the workbook.
+        //        WorksheetPart newWorksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+        //        newWorksheetPart.Worksheet = new Worksheet(new SheetData());
+        //        newWorksheetPart.Worksheet.Save();
+
+        //        Sheets sheets = workbookPart.Workbook.GetFirstChild<Sheets>() ?? workbookPart.Workbook.AppendChild(new Sheets());
+
+
+        //        // Append the new worksheet and associate it with the workbook.
+        //        Sheet sheet = new Sheet() { Name = name };
+        //        sheets.Append(sheet);
+        //        workbookPart.Workbook.Save();
+        //    }
+        //    return "test";
+        //}
+
+
+
+        static void InsertText(string docName, string text, string col, uint row)
+        {
+            using (SpreadsheetDocument spreadSheet = SpreadsheetDocument.Open(docName, true))
+            {
+                WorkbookPart workbookPart = spreadSheet.WorkbookPart ?? spreadSheet.AddWorkbookPart();
+                SharedStringTablePart shareStringPart;
+                if (workbookPart.GetPartsOfType<SharedStringTablePart>().Count() > 0)
+                {
+                    shareStringPart = workbookPart.GetPartsOfType<SharedStringTablePart>().First();
+                }
+                else
+                {
+                    shareStringPart = workbookPart.AddNewPart<SharedStringTablePart>();
+                }
+                int index = InsertSharedStringItem(text, shareStringPart);
+                Cell cell = InsertCellInWorksheet(col, row, workbookPart.WorksheetParts.First());
+                cell.CellValue = new CellValue(index.ToString());
+                cell.DataType = new EnumValue<CellValues>(CellValues.SharedString);
+                workbookPart.WorksheetParts.First().Worksheet.Save();
+            }
+        }
+
+        static int InsertSharedStringItem(string text, SharedStringTablePart shareStringPart)
+        {
+            if (shareStringPart.SharedStringTable is null)
+            {
+                shareStringPart.SharedStringTable = new SharedStringTable();
+            }
+            int i = 0;
+            foreach (SharedStringItem item in shareStringPart.SharedStringTable.Elements<SharedStringItem>())
+            {
+                if (item.InnerText == text)
+                {
+                    return i;
+                }
+
+                i++;
+            }
+            shareStringPart.SharedStringTable.AppendChild(new SharedStringItem(new DocumentFormat.OpenXml.Spreadsheet.Text(text)));
+            shareStringPart.SharedStringTable.Save();
+            return i;
+        }
+
+        static WorksheetPart InsertWorksheet(WorkbookPart workbookPart)
+        {
+            WorksheetPart newWorksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+            newWorksheetPart.Worksheet = new Worksheet(new SheetData());
+            newWorksheetPart.Worksheet.Save();
+            Sheets sheets = workbookPart.Workbook.GetFirstChild<Sheets>() ?? workbookPart.Workbook.AppendChild(new Sheets());
+            string relationshipId = workbookPart.GetIdOfPart(newWorksheetPart);
+            uint sheetId = 1;
+            if (sheets.Elements<Sheet>().Count() > 0)
+            {
+                sheetId = sheets.Elements<Sheet>().Select<Sheet, uint>(s =>
+                {
+                    if (!(s.SheetId is null || !s.SheetId.HasValue))
+                    {
+                        return s.SheetId.Value;
+                    }
+                    return 0;
+                }).Max() + 1;
+            }
+            string sheetName = "Sheet" + sheetId;
+            Sheet sheet = new Sheet() { Id = relationshipId, SheetId = sheetId, Name = sheetName };
+            sheets.Append(sheet);
+            workbookPart.Workbook.Save();
+            return newWorksheetPart;
+        }
+
+        static Cell InsertCellInWorksheet(string columnName, uint rowIndex, WorksheetPart worksheetPart)
+        {
+            Worksheet worksheet = worksheetPart.Worksheet;
+            var sheetData = worksheet.GetFirstChild<SheetData>();
+            string cellReference = columnName + rowIndex;
+            Row row;
+            if (sheetData?.Elements<Row>().Where(r => !(r.RowIndex is null || r.RowIndex != rowIndex)).Count() != 0)
+            {
+                row = sheetData.Elements<Row>().Where(r => !(r.RowIndex is null || r.RowIndex != rowIndex)).First();
+            }
+            else
+            {
+                row = new Row() { RowIndex = rowIndex };
+                sheetData.Append(row);
+            }
+            if (row.Elements<Cell>().Where(c => !(c.CellReference is null || c.CellReference.Value != columnName + rowIndex)).Count() > 0)
+            {
+                return row.Elements<Cell>().Where(c => !(c.CellReference is null || c.CellReference.Value != cellReference)).First();
+            }
+            else
+            {
+                Cell refCell = null;
+                foreach (Cell cell in row.Elements<Cell>())
+                {
+                    if (string.Compare(cell.CellReference?.Value, cellReference, true) > 0)
+                    {
+                        refCell = cell;
+                        break;
+                    }
+                }
+                Cell newCell = new Cell() { CellReference = cellReference };
+                row.InsertBefore(newCell, refCell);
+                worksheet.Save();
+                return newCell;
+            }
+        }
+
+
+        static uint GetNextEmptyCell(string FileName, string sheetName, string col)
+        {
+            bool inLoop = true;
+            uint rowIndex = 1;
+            string cellReference;
+            do
+            {
+                cellReference = col + rowIndex;
+                if (GetCellValue(FileName, sheetName, cellReference) != string.Empty)
+                {
+                    rowIndex++;
+                }
+                else
+                {
+                    inLoop = false;
+                }
+            }
+            while (inLoop);
+            return rowIndex;
+        }
+
+
+        static string GetCellValue(string fileName, string sheetName, string addressName)
+        {
+            string value = null;
+            using (SpreadsheetDocument document = SpreadsheetDocument.Open(fileName, false))
+            {
+                WorkbookPart wbPart = document.WorkbookPart;
+                Sheet theSheet = wbPart?.Workbook.Descendants<Sheet>().Where(s => s.Name == sheetName).FirstOrDefault();
+                if (theSheet is null || theSheet.Id is null)
+                {
+                    throw new ArgumentException("sheetName");
+                }
+                WorksheetPart wsPart = (WorksheetPart)wbPart.GetPartById(theSheet.Id);
+                Cell theCell = wsPart.Worksheet?.Descendants<Cell>()?.Where(c => c.CellReference == addressName).FirstOrDefault();
+                if (theCell is null || theCell.InnerText.Length < 0)
+                {
+                    return string.Empty;
+                }
+                value = theCell.InnerText;
+                if (theCell.DataType is null)
+                {
+                    return value;
+                }
+                if (theCell.DataType.Value == CellValues.SharedString)
+                {
+                    var stringTable = wbPart.GetPartsOfType<SharedStringTablePart>().FirstOrDefault();
+                    if (!(stringTable is null))
+                    {
+                        value = stringTable.SharedStringTable.ElementAt(int.Parse(value)).InnerText;
+                    }
+                }
+                else if (theCell.DataType.Value == CellValues.Boolean)
+                {
+                    switch (value)
+                    {
+                        case "0":
+                            value = "FALSE";
+                            break;
+                        default:
+                            value = "TRUE";
+                            break;
+                    }
+                }
+                return value;
+            }
+        }
     }
-
 }
-
-
